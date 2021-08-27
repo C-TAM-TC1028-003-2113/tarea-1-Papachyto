bfe533887f2d9de4e3526dc94cb103aebe0343ef
Papachyto <A01412712@itesm.mx>
2021-08-27 12:35:55 -0500

    Hice la tarea 3

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,9 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    nuevos = int(input("Dame la cantidad de juegos nuevos: "))
+    usados = int(input("Dame la cantidad de juegos usados: "))
+    costo = (1000*nuevos) + (350*usados)
+    print("El total de la compra es", costo)
 
 
 if __name__ == '__main__':
