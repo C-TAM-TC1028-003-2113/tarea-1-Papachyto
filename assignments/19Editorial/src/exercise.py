d5285bccb11f613760d938be90ba335c3ad6a17a
Papachyto <A01412712@itesm.mx>
2021-08-27 12:50:09 -0500

    Hice la Tarea 5

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    import math
+    palabras = int( input( "Dame el número de palabras: "))
+    paginas = math.ceil(palabras / 475)
+    costo = paginas * 60 * 0.9
+    print( "El costo de la publicación es:", costo)
 
 
 if __name__ == '__main__':
